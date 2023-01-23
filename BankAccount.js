9b1f6771865482530708f4b73f4199de5a4eddce
m <32762403+m-codes@users.noreply.github.com>
2023-01-23 13:53:10 +0000

    more tests + edge cases for the withdraw logic

--- BankAccount.js
+++ BankAccount.js
@@ -3,18 +3,27 @@ class BankAccount {
     this.balance = 0;
   }
 
-  deposit(amount){
-    if (typeof(amount) === "number" && amount > 0){
-      this.balance += amount
+  deposit(amount) {
+    if (typeof amount === "number" && amount > 0) {
+      this.balance += amount;
     } else {
-      throw new Error("The amount to deposit must be a number greater than 0.")
+      throw new Error("Deposit amount must be a number greater than 0.");
     }
   }
 
-  printBalance(){
-    return this.balance;
+  withdraw(amount) {
+    if (typeof amount === "number" && amount > 0 && amount <= this.balance) {
+      this.balance -= amount;
+    } else {
+      throw new Error(
+        "Withdrawal amount must be a positive number that do not exceed the balance."
+      );
+    }
   }
 
+  printBalance() {
+    return this.balance;
+  }
 }
 
 module.exports = BankAccount;
--- BankAccount.test.js
+++ BankAccount.test.js
@@ -1,28 +1,84 @@
 const BankAccount = require("./BankAccount");
 
 describe("Bank Account", () => {
+  // balance tests
   it("returns the initial balance", () => {
     const account = new BankAccount();
     expect(account.printBalance()).toEqual(0);
   });
 
+  // deposit tests
   it("returns the balance after a single deposit", () => {
     const account = new BankAccount();
-    account.deposit(10)
+    account.deposit(10);
     expect(account.printBalance()).toEqual(10);
   });
 
   it("returns the balance after multiple deposits", () => {
     const account = new BankAccount();
-    account.deposit(10)
-    account.deposit(5)
+    account.deposit(10);
+    account.deposit(5);
     expect(account.printBalance()).toEqual(15);
   });
 
-  it("returns the balance after an incorrect deposit", () => {
+  it("throws an error when given invalid deposit amount", () => {
     const account = new BankAccount();
-    expect(() => account.deposit(-2)).toThrow("The amount to deposit must be a number greater than 0.");
+    expect(() => account.deposit(-2)).toThrow(
+      "Deposit amount must be a number greater than 0."
+    );
     expect(account.printBalance()).toEqual(0);
   });
-});
 
+  it("throws an error when given invalid deposit type", () => {
+    const account = new BankAccount();
+    expect(() => account.deposit("hello")).toThrow(
+      "Deposit amount must be a number greater than 0."
+    );
+    expect(account.printBalance()).toEqual(0);
+  });
+
+  // withdraw tests
+  it("returns the balance after a withdrawal", () => {
+    const account = new BankAccount();
+    account.deposit(10);
+    account.withdraw(5);
+    expect(account.printBalance()).toEqual(5);
+  });
+
+  it("returns the balance after mutiple withdrawals", () => {
+    const account = new BankAccount();
+    account.deposit(10);
+    account.withdraw(5);
+    account.withdraw(3);
+    expect(account.printBalance()).toEqual(2);
+  });
+
+  it("throws an error when given invalid withdrawal amount", () => {
+    const account = new BankAccount();
+    expect(() => account.withdraw(-10)).toThrow(
+      "Withdrawal amount must be a positive number that do not exceed the balance."
+    )
+  });
+
+  it("throws an error when given invalid withdrawal type", () => {
+    const account = new BankAccount();
+    expect(() => account.withdraw("hello")).toThrow(
+      "Withdrawal amount must be a positive number that do not exceed the balance."
+    )
+  });
+
+  it("throws an error when withdrawal is greater than the balance", () => {
+    const account = new BankAccount();
+    account.deposit(50);
+    expect(() => account.withdraw(80)).toThrow(
+      "Withdrawal amount must be a positive number that do not exceed the balance."
+    )
+  });
+
+  it("returns the balance as 0 when the user withdraw the full amount on their account", () => {
+    const account = new BankAccount();
+    account.deposit(50);
+    account.withdraw(50)
+    expect(account.printBalance()).toEqual(0)
+  });
+});
